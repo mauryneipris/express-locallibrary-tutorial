d09ff27d791524786d7a4dc1f3f11a2b9893fbe3
Maury Neipris <mauryneipris@Maurys-MBP.local>
2020-01-11 14:53:28 -0500

    First version of app moved to github repo

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.send('respond with a resource and so on');
+});
+router.get('/cool', function(req, res) {
+  res.send("You're so cool");
+});
+
+module.exports = router;
